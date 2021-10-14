b56415cee0c843a7a113abafcc7b347616c7a354
mxs3203 <mxs3203@g.rit.edu>
2021-10-14 12:13:39 +0200

    conv example and folder refactoring

---
+++ Conv_Example/w_loss/main_w_loss.py
@@ -0,0 +1,117 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision.datasets import MNIST
+from torchvision.transforms import transforms
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+from GAN.Conv_Example.Critic import Critic
+from GAN.Conv_Example.Generator import Generator, get_noise
+from GAN.Conv_Example.utils import show_tensor_images
+from GAN.Conv_Example.utils_for_w_loss import gradient_penalty, get_gradient, get_crit_loss, get_gen_loss
+
+n_epochs = 100
+z_dim = 64
+display_step = 50
+batch_size = 128
+lr = 0.0002
+beta_1 = 0.5
+beta_2 = 0.999
+c_lambda = 10
+crit_repeats = 5
+device = 'cuda'
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,)),
+])
+
+dataloader = DataLoader(
+    MNIST('../', download=False, transform=transform),
+    batch_size=batch_size,
+    shuffle=True)
+
+gen = Generator(z_dim).to(device)
+gen_opt = torch.optim.Adam(gen.parameters(), lr=lr, betas=(beta_1, beta_2))
+crit = Critic().to(device)
+crit_opt = torch.optim.Adam(crit.parameters(), lr=lr, betas=(beta_1, beta_2))
+
+def weights_init(m):
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+    if isinstance(m, nn.BatchNorm2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+        torch.nn.init.constant_(m.bias, 0)
+gen = gen.apply(weights_init)
+crit = crit.apply(weights_init)
+
+cur_step = 0
+generator_losses = []
+critic_losses = []
+for epoch in range(n_epochs):
+    # Dataloader returns the batches
+    for real, _ in tqdm(dataloader):
+        cur_batch_size = len(real)
+        real = real.to(device)
+
+        mean_iteration_critic_loss = 0
+        for _ in range(crit_repeats):
+            ### Update critic ###
+            crit_opt.zero_grad()
+            fake_noise = get_noise(cur_batch_size, z_dim, device=device)
+            fake = gen(fake_noise)
+            crit_fake_pred = crit(fake.detach())
+            crit_real_pred = crit(real)
+
+            epsilon = torch.rand(len(real), 1, 1, 1, device=device, requires_grad=True)
+            gradient = get_gradient(crit, real, fake.detach(), epsilon)
+            gp = gradient_penalty(gradient)
+            crit_loss = get_crit_loss(crit_fake_pred, crit_real_pred, gp, c_lambda)
+
+            # Keep track of the average critic loss in this batch
+            mean_iteration_critic_loss += crit_loss.item() / crit_repeats
+            # Update gradients
+            crit_loss.backward(retain_graph=True)
+            # Update optimizer
+            crit_opt.step()
+        critic_losses += [mean_iteration_critic_loss]
+
+        ### Update generator ###
+        gen_opt.zero_grad()
+        fake_noise_2 = get_noise(cur_batch_size, z_dim, device=device)
+        fake_2 = gen(fake_noise_2)
+        crit_fake_pred = crit(fake_2)
+
+        gen_loss = get_gen_loss(crit_fake_pred)
+        gen_loss.backward()
+
+        # Update the weights
+        gen_opt.step()
+
+        # Keep track of the average generator loss
+        generator_losses += [gen_loss.item()]
+
+        ### Visualization code ###
+        if cur_step % display_step == 0 and cur_step > 0:
+            gen_mean = sum(generator_losses[-display_step:]) / display_step
+            crit_mean = sum(critic_losses[-display_step:]) / display_step
+            print(f"Step {cur_step}: Generator loss: {gen_mean}, critic loss: {crit_mean}")
+            show_tensor_images(fake)
+            show_tensor_images(real)
+            step_bins = 20
+            num_examples = (len(generator_losses) // step_bins) * step_bins
+            plt.plot(
+                range(num_examples // step_bins),
+                torch.Tensor(generator_losses[:num_examples]).view(-1, step_bins).mean(1),
+                label="Generator Loss"
+            )
+            plt.plot(
+                range(num_examples // step_bins),
+                torch.Tensor(critic_losses[:num_examples]).view(-1, step_bins).mean(1),
+                label="Critic Loss"
+            )
+            plt.legend()
+            plt.show()
+
+        cur_step += 1
---
+++ FF_Example/utils.py
@@ -0,0 +1,82 @@
+import torch
+from torch import nn
+from tqdm.auto import tqdm
+from torchvision import transforms
+from torchvision.datasets import MNIST # Training dataset
+from torchvision.utils import make_grid
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+torch.manual_seed(0) # Set for testing purposes, please do not change!
+
+def show_tensor_images(image_tensor, num_images=25, size=(1, 28, 28)):
+    '''
+    Function for visualizing images: Given a tensor of images, number of images, and
+    size per image, plots and prints the images in a uniform grid.
+    '''
+    image_unflat = image_tensor.detach().cpu().view(-1, *size)
+    image_grid = make_grid(image_unflat[:num_images], nrow=5)
+    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+    plt.show()
+
+# UNQ_C3 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+# GRADED FUNCTION: get_noise
+def get_noise(n_samples, z_dim, device='cpu'):
+    '''
+    Function for creating noise vectors: Given the dimensions (n_samples, z_dim),
+    creates a tensor of that shape filled with random numbers from the normal distribution.
+    Parameters:
+        n_samples: the number of samples to generate, a scalar
+        z_dim: the dimension of the noise vector, a scalar
+        device: the device type
+    '''
+    # NOTE: To use this on GPU with device='cuda', make sure to pass the device
+    # argument to the function you use to generate the noise.
+    #### START CODE HERE ####
+    return torch.randn(n_samples, z_dim, device=device)
+    #### END CODE HERE ####
+
+
+# UNQ_C4 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+# GRADED FUNCTION: get_discriminator_block
+def get_discriminator_block(input_dim, output_dim):
+    '''
+    Discriminator Block
+    Function for returning a neural network of the discriminator given input and output dimensions.
+    Parameters:
+        input_dim: the dimension of the input vector, a scalar
+        output_dim: the dimension of the output vector, a scalar
+    Returns:
+        a discriminator neural network layer, with a linear transformation
+          followed by an nn.LeakyReLU activation with negative slope of 0.2
+          (https://pytorch.org/docs/master/generated/torch.nn.LeakyReLU.html)
+    '''
+    return nn.Sequential(
+        #### START CODE HERE ####
+         nn.Linear(input_dim, output_dim), #Layer 1
+         nn.LeakyReLU(0.2, inplace=True)
+        #### END CODE HERE ####
+    )
+
+# UNQ_C1 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+# GRADED FUNCTION: get_generator_block
+def get_generator_block(input_dim, output_dim):
+    '''
+    Function for returning a block of the generator's neural network
+    given input and output dimensions.
+    Parameters:
+        input_dim: the dimension of the input vector, a scalar
+        output_dim: the dimension of the output vector, a scalar
+    Returns:
+        a generator neural network layer, with a linear transformation
+          followed by a batch normalization and then a relu activation
+    '''
+    return nn.Sequential(
+        # Hint: Replace all of the "None" with the appropriate dimensions.
+        # The documentation may be useful if you're less familiar with PyTorch:
+        # https://pytorch.org/docs/stable/nn.html.
+        #### START CODE HERE ####
+        nn.Linear(input_dim,output_dim),
+        nn.BatchNorm1d(output_dim),
+        #### END CODE HERE ####
+        nn.ReLU(inplace=True)
+    )
